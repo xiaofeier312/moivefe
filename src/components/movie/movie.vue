f36b13e99bab047b5da28810f56f68c98fc9a459
zhouzhijun <zhouzhijun@sunlands.com>
2020-04-04 23:22:03 +0800

    transform data between parent and son componets

---
+++ src/components/movie/movie.vue
@@ -0,0 +1,80 @@
+<template>
+  <div>
+    <p>movie text</p>
+    <p>{{msg}}</p>
+    <button @click="clickbtn" data-id="333">btn</button>
+    <p>btnValue: {{btnValue}}</p>
+    <p v-show="displayP">shencang</p>
+    <input v-model="username" />
+    <p>myname: {{username}}</p>
+    <p>点击次数: {{countd}}</p>
+    <button @click="countd+=1">Click</button>
+    <li v-for="item,index in itemlist">{{index}} {{item}}</li>
+    <br />
+    <hr />
+    <productItem 
+    :product-list="productList" 
+    v-on:childproductitem="getchildvalid"
+    ></productItem>
+    <hr />
+    {{childval}}
+  </div>
+</template>
+
+<script>
+import productItem from "../productItem/productItem";
+
+export default {
+  name: "movie",
+  data() {
+    return {
+      msg: "this is msg",
+      btnValue: 0,
+      displayP: false,
+      countd: 0,
+      username: "",
+      itemlist: ["time1", "time2", "time3", "time4"],
+      productList: [
+        {
+          name: "牙刷",
+          price: 23
+        },
+        {
+          name: "刮胡刀",
+          price: 12.2
+        },
+        {
+          name: "西瓜",
+          price: 333.1
+        }
+      ],
+      childval: ''
+    };
+  },
+  components: {
+    productItem
+  },
+  created() {
+    console.log(this);
+  },
+  methods: {
+    clickbtn(e) {
+      console.log("--e:");
+      console.log(e);
+      this.btnValue = parseInt(e.target.dataset.id);
+      if (this.btnValue > 10) {
+        this.btnValue = this.btnValue - 1;
+        this.displayP = true;
+      }
+    },
+    getchildvalid(val) {
+      console.log(' -- get val form child:')
+      console.log(val)
+      this.childval = val
+    }
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/productItem/productItem.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <p>item page</p>
+    <li v-for="item,index in productList">
+      {{ index}}
+      {{item.name}}
+    </li>
+    <button @click="getthename">what?</button>
+    <br>
+    {{ num }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "productItem",
+  props: ["productList"],
+  data() {
+    return {
+     num: 0
+    };
+  },
+  created() {
+    //console.log(productList)
+  },
+  methods: {
+    getthename() {
+      console.log(this.productList);
+      this.num += 1
+      console.log(this.num)
+      this.$emit("childproductitem",this.num)
+    }
+  }
+};
+</script>
+
+<style>
+</style>
